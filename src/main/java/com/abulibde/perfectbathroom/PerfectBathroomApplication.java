0c0933e4ae3313552e86c0c9d1830ab32bd1d174
Abu <iliyanananan@gmail.com>
2023-11-11 14:17:24 +0200

    test commit

---
+++ src/main/java/com/abulibde/perfectbathroom/PerfectBathroomApplication.java
@@ -0,0 +1,13 @@
+package com.abulibde.perfectbathroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectBathroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfectBathroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abulibde/perfectbathroom/PerfectBathroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abulibde.perfectbathroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfectBathroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
